f692f78baa2266aa8042bc19a59a96a915ff2b91
VisonaN <paulovisona@gmail.com>
2023-10-17 15:11:49 -0300

    JS Otávio Miranda

---
+++ aula3/ex/index.js
@@ -0,0 +1,4 @@
+let hora = 15
+console.log(`Meu nome é "Paulo Visoná Neto". Estou aprendendo JavaScript às ${hora} da tarde.`);
+// Ou
+console.log('Meu nome é "Paulo Visoná Neto". Estou aprendendo JavaScript às', 15, 'da Tarde.');
---
+++ aula3/index.js
@@ -0,0 +1,19 @@
+// Exibindo textos 
+
+console.log('Paulo "Visoná"'); // String = Texto
+console.log("Paulo 'Visoná'"); // String
+console.log(`'Paulo' "Visoná" `); // String
+// Entre crases `` é template string
+
+// Exibindo Números (Number)
+
+console.log(123); // Number
+console.log(456); // Number
+console.log(789); // Number
+
+console.log(12.2); // Number
+console.log(12.3); // Number
+console.log(12.4); // Number
+
+console.log(12, 13.5, 'Paulo Visoná'); 
+
