c3899e3a66bd55780478fa9ab29bc35c69578145
VisonaN <paulovisona@gmail.com>
2023-10-17 15:31:00 -0300

    Comentários em JS

---
+++ aula4/comentarios.js
@@ -0,0 +1,16 @@
+// Comentários são trechos de código ignorados pelo interpretador.
+// São usados para explicar o código feito: Usados nas documentação do código.
+// são comentários de uma linha <=
+console.log('Olá, Mundo!'); // Aqui tem outro comentário.
+console.log('Linha 1');
+console.log('Linha 2');
+// Mais um comentário
+//console.log('Linha 3'); <= colocar duas barras antes do código para que ele não seja executado quando você quiser testar algo e ignorar uma linha de código específica
+console.log('Linha 4');
+// Vai ser ignorado
+/* Para comentários de mais de uma linha usar <= */
+
+/* Usar comentários em bloco para ignorar mais de uma linha de código
+console.log('Linha 4');
+console.log('Linha 4');
+*/
