12ee9ab65e6f8136d56b1ba99e60fa737586c163
VisonaN <paulovisona@gmail.com>
2023-10-18 14:20:26 -0300

    diferença entre var e let

---
+++ aula9/index.js
@@ -0,0 +1,11 @@
+// ECMAScript => Padronização da linguagem JS feita pela ECMA
+// ECMA 2015 (ES6) => Versão da documentação
+// nome = 'Paulo' => Não declarar variáveis sem var ou let ou const pois assim é criada uma variável global e pode dar erro no sistema.
+// var nome = 'Paulo Visoná';
+// var nome = 'Visona Paulo';
+/*
+AO USAR LET PARA CRIAR UMA VARIÁVEL, PASSA A SER IMPOSSÍVEL DECLARAR A MESMA VARIÁVEL COM OUTRO VALOR COMO ACONTECE AO USAR 'VAR'
+let nome = 'Paulo Visoná';
+let nome = 'Neto';
+console.log(nome);
+*/
