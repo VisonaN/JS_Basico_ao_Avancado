3c9e8732a727ff9bb2684e624894259c08449100
VisonaN <paulovisona@gmail.com>
2023-10-18 10:19:26 -0300

    Iniciei constantes em JS

---
+++ aula7/index.js
@@ -0,0 +1,14 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não pode conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode declarar uma constante sem inicializar ela =>
+// => const nome; Vai dar erro
+// Não pode modificar o valor de uma constante =>
+// => nome = 'Visoná' Vai dar erro
+// NÃO UTILIZE VAR, UTILIZE CONST
+
+const nome = 'Paulo';
+console.log(nome)
