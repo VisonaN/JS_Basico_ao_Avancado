32bc865e99d250c39a28b66097ab303efdd713ce
VisonaN <paulovisona@gmail.com>
2023-10-18 15:56:18 -0300

    operadores de atribuição

--- aula11/index.js
+++ aula11/index.js
@@ -13,13 +13,19 @@
 * * / % => Esquerda para direita
 * + - => Esquerda para direita
 
+* OPERADORES DE ATRIBUIÇÃO
+* +=
+* *=
+* **=
+* -=
 */
 
 // let contador = 10;  Não pode usar const pois vai dar erro devido ao fato de uma constante não permitir que altere o seu valor
 // contador++;  => ++ (operador de incremento) adiciona de um em um (pode ser usado antes ou depois => ++contador ou contador++)
 // contador--;  => -- (operador de decremento) subtrai de um em um (pode ser usado antes ou depois => --contador ou contador--)
-const passo = 2;
-let contador = 0;
+// const passo = 5;
+// let contador = 0;
+/*
 contador = contador + passo;
 console.log(contador);
 contador = contador + passo;
@@ -30,3 +36,11 @@ contador = contador + passo;
 console.log(contador);
 contador = contador + passo;
 console.log(contador);
+*/
+// Atalho para não ter que ficar escrevendo esse monte de código para ficar somando
+
+// contador += passo;  contador = contador + passo
+
+const num1 = 10;
+const num2 = 'Paulo'; // Mas se tiver um número entre aspas o JS vai conseguir interpretar e fazer a conta
+console.log(num1 * num2); // Resulta em NaN (Not a Number)
