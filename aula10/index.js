a30e48a9e09ae1df6e8038a854384232ac741626
VisonaN <paulovisona@gmail.com>
2023-10-18 14:44:44 -0300

    dados primitivos em JS

---
+++ aula10/index.js
@@ -0,0 +1,13 @@
+// DADOS PRIMITIVOS EM JS
+
+const nome = 'Paulo'; // String
+const nome1 = "Paulo"; // String
+const nome2 = `Paulo`; // String
+const num = 10; // Number
+const num1 = 10.52; // Number
+let nomeAluno; // Undefined = Não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo = > Não aponta para local nenhum na memória
+// Null e Undefined não são iguais =>
+// => Quando utilizar nulo é porque explicitamente você quer que tenha esse valor não apontando em um lugar na memória
+const aprovado = true; // Boolean = true ou false (valor lógico)
+// Também tem o tipo de dado symbol que vai ser abordado mais a frente
