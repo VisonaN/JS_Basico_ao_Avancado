b105d35accd3dc70dd9e618637dcf64f150499ee
VisonaN <paulovisona@gmail.com>
2023-10-20 15:27:20 -0300

    Fiz o exercício de números sozinho

--- aula16/ex/js/index.js
+++ aula16/ex/js/index.js
@@ -1,17 +1,19 @@
 const numero = Number(prompt('Digite um número'));
 const numeroTitulo = window.document.getElementById('numero-titulo');
 const texto = window.document.getElementById('texto');
-const inteiro = window.document.getElementById('inteiro');
-const nan = window.document.getElementById('nan');
-const cima = window.document.getElementById('cima');
-const baixo = window.document.getElementById('baixo');
-const dec = window.document.getElementById('dec');
+// NÃO PRECISA DISSO TUDO DE CONST
+// const inteiro = window.document.getElementById('inteiro');
+// const nan = window.document.getElementById('nan');
+// const cima = window.document.getElementById('cima');
+// const baixo = window.document.getElementById('baixo');
+// const dec = window.document.getElementById('dec');
 
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Raiz quadrada: ${numero ** 0.5}.</p>`;
-inteiro.innerHTML = `<p> ${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
-nan.innerHTML = `<p>É NaN: ${Number.isNaN(numero)}</p>`;
-cima.innerHTML = `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`
-baixo.innerHTML = `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`
-dec.innerHTML = `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}.</p>`;
+texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`
