082387fb361ac1acf5ecc9230c32e9d8ccb79e3e
VisonaN <paulovisona@gmail.com>
2023-11-07 08:59:38 -0300

    closures

--- aula39_funções/closures.js
+++ aula39_funções/closures.js
@@ -1,1 +1,24 @@
 //                                               CLOSURES
+// É a habilidade da função de acessar o seu escopo léxico
+// function retornaFuncao() {
+//     const nome = 'Luiz';
+//     return function() {
+//         return nome;
+//     }
+// }
+
+// const funcao = retornaFuncao();
+// console.dir(funcao);
+
+function retornaFuncao(nome) {
+    return function() {
+        return nome;
+    }
+}
+
+const funcao = retornaFuncao('Paulo');
+const funcao2 = retornaFuncao('Visoná');
+console.dir(funcao);
+console.dir(funcao2);
+console.log(funcao());
+console.log(funcao2());
