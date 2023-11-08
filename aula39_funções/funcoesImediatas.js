0ae8f923a1761a330588ef625445542314c9a820
VisonaN <paulovisona@gmail.com>
2023-11-08 15:53:06 -0300

    atualizes as funções imediatas e criei as funções fábrica

--- aula39_funções/funcoesImediatas.js
+++ aula39_funções/funcoesImediatas.js
@@ -1,2 +1,32 @@
-//                                       FUNÇÕES IMEDIATAS (IIFE)
+// FUNÇÕES IMEDIATAS / FUNÇÕES AUTO INVOCADAS (IIFE) - Immediatly Invoked Function Expression
+
+// Uma forma de fugir do escopo global seria colocar tudo dentro de uma função
+// function qualqueCoisa() { 
+//     console.log(123456);
+// }
+// qualqueCoisa();
+
+
+// Abaixo temos uma IIFE (Que faz a proteção do que for escrito dentro dela não ser afetado ou afetar o escopo global) - Precisa ser envolvida entre parênteses e chamar os parênteses no final ->
+/* (function() {
+
+})();
+*/
+(function(idade, peso, altura) {
+    // const nome = 'Paulo'; - Pode colocar qualquer coisa dentro que não vai ser afetado pelo escopo global
+    // console.log(nome)
+
+    const sobrenome = 'Visoná';
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('Paulo'))
+    }
+
+    falaNome();
+    console.log(idade, peso, altura)
+
+})(27, 58, 1.65);
 
